e8a23fb8af5955fdcfb309e38da4a6c1478d93c3
Kofi <pastorkofi101@gmail.com>
2024-11-17 07:51:41 -0500

    Work on two excersise problems.

--- problems/01-car-drive.js
+++ problems/01-car-drive.js
@@ -1,5 +1,19 @@
 // Your code here
+class Car {
+	constructor(speed) {
+		this.speed = speed;
+	}
+	drive(newSpeed) {
+		this.speed = newSpeed;
+		return this.speed;
+	}
+}
 
+// let car = new Car();
+// console.log(car.drive(0));
+// console.log(car);
+// console.log(car.drive(15));
+// console.log(car);
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
